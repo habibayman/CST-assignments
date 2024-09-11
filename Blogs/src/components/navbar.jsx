438c7cd3bea80a61c6312278d0f8844a8b8a4572
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2024-09-11 06:07:38 +0300

    feat: (Blogs | React.js) (#1)
    * feat: (Blogs) -init
    
    * feat: update styles
    
    * fix: unnecessary errors/warnings
    
    * update dependencies
    
    * fix: complete TODOs
    
    * fix: complete TODOs

---
+++ Blogs/src/components/navbar.jsx
@@ -0,0 +1,42 @@
+import './navbar.css';
+import axios from 'axios';
+
+const Navbar = ({ posts, setPosts }) => {
+  const addPost = async () => {
+    const new_post = {
+      id: posts.length + 99, // the API uses elem index as its id ==> adding only 1 will lead to id duplications
+      title: 'New Post',
+      body: 'New Post Body',
+    };
+    try {
+      axios
+        .post('https://jsonplaceholder.typicode.com/posts', new_post)
+        .then((response) => {
+          setPosts([new_post, ...posts]);
+          console.log('response ==========> ', response); /// DEBUG
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <div id="logo">
+        <img
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogospng.org%2Fdownload%2Freact%2Flogo-react-1024.png&f=1&nofb=1&ipt=7214a431bfb13e49842c502d3fec5b8e89d3c83e6d285c9eb146bac546229832&ipo=images"
+          alt="W3Schools.com"
+          width="100"
+        />
+      </div>
+
+      <ul id="right-content">
+        <button onClick={addPost} className="add-post">
+          Add Post
+        </button>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ Blogs/src/pages/posts.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './posts.css';
+import Navbar from '../components/navbar';
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]); // => must initialize to be used as an array
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts')
+          .then((response) => {
+            setPosts(response.data);
+          });
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      }
+    };
+    getPosts();
+  }, []);
+
+  const deletePost = async (id) => {
+    if (
+      window.confirm(
+        'Are you sure you want to delete this post?\n ⚠️This action cannot be undone⚠️'
+      )
+    ) {
+      try {
+        setPosts(posts.filter((post) => post.id !== id));
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      }
+    }
+  };
+
+  if (error) return <div>failed to load</div>; // when there is an error in fetching data from api, it will show failed to load
+  if (!posts) return <div>loading...</div>; // when data is being fetched from api, it will show loading...
+
+  console.log('posts ======> ', posts); // DEBUG
+  return (
+    <div>
+      <Navbar posts={posts} setPosts={setPosts} />
+      <div className="grid-container">
+        {posts &&
+          // shouldn't I not change the state directly? 🤔
+          posts.map((post) => {
+            return (
+              <div className="grid-item" key={post.id}>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+                <div>
+                  <button
+                    className="delete-button"
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    Delete Post
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
