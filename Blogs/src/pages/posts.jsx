a86ecccf62f70cc8aa6f139dc777670f51edb1ab
Habiba Ayman <HabibaAyman2004@outlook.com>
2024-09-11 06:02:31 +0300

    fix: complete TODOs

--- Blogs/src/pages/posts.jsx
+++ Blogs/src/pages/posts.jsx
@@ -4,53 +4,71 @@ import './posts.css';
 import Navbar from '../components/navbar';
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]); // => must initialize to be used as an array
+  const [error, setError] = useState(null);
 
-    // TODO : create posts state using useState hook and name them posts, setPosts
-    // TODO : create error state using useState hook and name them error, setError
-    
-    useEffect(() => {
-        const getPosts = async () => {
-            try {
-                // TODO : get posts from https://jsonplaceholder.typicode.com/posts and set them using setPosts
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts')
+          .then((response) => {
+            setPosts(response.data);
+          });
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      }
+    };
+    getPosts();
+  }, []);
 
-            } catch (error) {
-                // TODO : set error using setError
-                setError(error);
-
-                console.log(error);
-            }
-        }
-        getPosts();
-    }, []);
-
-    const deletePost = async (id) => {
-        try {
-            // TODO : delete post with given id from https://jsonplaceholder.typicode.com/posts 
-            // and set posts using setPosts and filter method to filter out deleted post(Hint : post => post.id !== id)
-        } catch (error) {
-            // TODO : set error using setError
-            console.log(error);
-        }
+  const deletePost = async (id) => {
+    if (
+      window.confirm(
+        'Are you sure you want to delete this post?\n ⚠️This action cannot be undone⚠️'
+      )
+    ) {
+      try {
+        setPosts(posts.filter((post) => post.id !== id));
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      }
     }
+  };
 
+  if (error) return <div>failed to load</div>; // when there is an error in fetching data from api, it will show failed to load
+  if (!posts) return <div>loading...</div>; // when data is being fetched from api, it will show loading...
 
-    if (error) return <div>failed to load</div>; // when there is an error in fetching data from api, it will show failed to load
-    if (!posts) return <div>loading...</div>; // when data is being fetched from api, it will show loading...
-    return (
-        <div>
-            <Navbar/> {/* TODO : pass posts and setPosts as props to Navbar component */}
-            <div className='grid-container' >
-                {/* TODO : map over posts and put grid-item div inside it */}
-                    <div className="grid-item" key={post.id}>
-                        <h1>{post.title}</h1>
-                        <p>{post.body}</p>
-                        <div>
-                            <button className="delete-button" onClick={() => deletePost(post.id)}>Delete Post</button>
-                        </div>
-                    </div>
-            </div>
-        </div>
-    );
-   }
+  console.log('posts ======> ', posts); // DEBUG
+  return (
+    <div>
+      <Navbar posts={posts} setPosts={setPosts} />
+      <div className="grid-container">
+        {posts &&
+          // shouldn't I not change the state directly? 🤔
+          posts.map((post) => {
+            return (
+              <div className="grid-item" key={post.id}>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+                <div>
+                  <button
+                    className="delete-button"
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    Delete Post
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
-    export default Posts;
+export default Posts;
