38f4e45747f5190c935b0dd7b0476328249a0951
Habiba Ayman <HabibaAyman2004@outlook.com>
2024-09-16 05:08:44 +0300

    feat: created using observer pattern

---
+++ File-Watcher/FolderMonitor.js
@@ -0,0 +1,50 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const fsPromises = require('node:fs/promises');
+require('log-timestamp'); // knew there had to be a better way than hardcoding the date stamp 🤮 https://stackoverflow.com/questions/18814221/adding-timestamps-to-all-console-messages
+
+class FolderMonitor {
+  constructor(folderPath) {
+    this.folderPath = folderPath;
+    this.observers = [];
+    this.watcher = null;
+    this.changeHistory = []; // Keep store to create a text log file
+  }
+
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
+
+  // Recursive => to keep watching for subfolders
+  startMonitoring() {
+    this.watcher = fs.watch(
+      this.folderPath,
+      { recursive: true },
+      (eventType, filename) => {
+        const fullPath = path.join(this.folderPath, filename);
+        if (eventType === 'rename' && !fs.existsSync(fullPath)) {
+          this.notifyObservers('delete', fullPath);
+        } else {
+          this.notifyObservers(eventType, fullPath);
+        }
+      }
+    );
+  }
+
+  notifyObservers(eventType, filePath) {
+    this.observers.forEach(
+      (observer) =>
+        this.changeHistory.push(observer.update(eventType, filePath)) // prob breaks SRP
+    );
+  }
+
+  writeHistory() {
+    fsPromises
+      .writeFile('changeLog.txt', this.changeHistory.join('\n'))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = FolderMonitor;
